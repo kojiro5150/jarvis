a48b11400aeca4033c0f8242ae65c1bc6ca91df0
kojiro5150 <42211188+kojiro5150@users.noreply.github.com>
2026-07-28 21:55:23 +1000

    feat(eos): add canonical routing invocation handoff

--- lib/executive-operating-system/executive-capabilities/invocation/index.ts
+++ lib/executive-operating-system/executive-capabilities/invocation/index.ts
@@ -1,4 +1,6 @@
 export { CAPABILITY_INVOCATION_CONTRACT_VERSION } from "./types";
-export type { JsonValue,ImplementationStatus,ExecutionClass,InvocationLifecycleState,InvocationFailureCategory,InvocationStage,CapabilityRoutingPlan,ExecutionPolicy,CapabilityInvocationProvenance,CapabilityInvocationContext,CapabilityExecutionResult,ExecutiveCapabilityImplementationDescriptor,ExecutiveCapabilityImplementation,InvocationLifecycleTransition,CapabilityInvocationRecord,CapabilityInvocationFailure,CapabilityInvocationResult,CapabilityInvocationRequest } from "./types";
+export type { JsonValue,ImplementationStatus,ExecutionClass,InvocationLifecycleState,InvocationFailureCategory,InvocationStage,CapabilityRoutingPlan,CapabilityInvocationEnvelope,ExecutionPolicy,CapabilityInvocationProvenance,CapabilityInvocationContext,CapabilityExecutionResult,ExecutiveCapabilityImplementationDescriptor,ExecutiveCapabilityImplementation,InvocationLifecycleTransition,CapabilityInvocationRecord,CapabilityInvocationFailure,CapabilityInvocationResult,CapabilityInvocationRequest } from "./types";
 export { ExecutiveCapabilityImplementationRegistry } from "./registry";
 export { ExecutiveCapabilityInvoker,createExecutionPolicy } from "./invoker";
+export { ExecutiveCapabilityInvocationHandoff } from "./handoff";
+export type { CapabilityInvocationHandoffInput } from "./handoff";
